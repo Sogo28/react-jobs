22a48e400caea22c1978a4924af75e3b9548e206
Daniel Andre Sogo DIEME <danielandresogodieme@esp.sn>
2024-08-10 01:05:28 +0000

    removing some useless lines

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,9 +1,7 @@
 import { login } from "../../services/api/AuthApi";
-import { useMutation, useQueryClient, UseMutationResult } from "@tanstack/react-query";
+import { useMutation, UseMutationResult } from "@tanstack/react-query";
 import { LoginFormFieldsType } from "../../schemas/LoginFormFieldsSchema";
 export default function useLogin(): UseMutationResult<string, Error, LoginFormFieldsType> {
-
-  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: login,
